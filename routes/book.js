594f76f972f929cbf713a80804a071671e9c04a1
itachi1994 <you@example.com>
2020-04-07 03:31:20 -0400

    Search API Modified

--- routes/book.js
+++ routes/book.js
@@ -9,6 +9,8 @@ const book = require('../models/book.schema')
 })*/
 const bookRouter = express.Router();
 
+
+/*Post a book*/
 bookRouter.route('/Addbook').post(async (req, res) => {
     const body = req.body;
     let checkBook = await book.find({title: body.title});
@@ -20,6 +22,33 @@ bookRouter.route('/Addbook').post(async (req, res) => {
         .then(()=>res.json({status:'Book added!!'}))
         .catch(err=>res.status(400).json('Error: '+err))
 })
+
+/*Get all books*/
+bookRouter.route('/getAllBooks').get((req, res) => {
+    book.find()
+        .then(books => res.json(books))
+        .then(() => res.json({status: 'Showing all Books'}))
+        .catch(err => res.status(400).json('Error: '+err))
+})
+
+/*Get book by ID*/
+bookRouter.route('/getBookById/:bid').get((req, res) => {
+    const bookId = req.params['bid'];
+    book.findById(bookId)
+        .then(book => res.json(book))
+        .catch(err => res.status(400).json('Error: '+err))
+})
+
+bookRouter.route('/search').get((req, res) => {
+    let query = req.query.q;
+    book.find({$or:[{title: {"$regex": query,"$options":"i"}},
+            {authors:{"$elemMatch":{"$regex":query,"$options":"i"}}},{category:{"$elemMatch":{"$regex":query,"$options":"i"}}}]})
+        .then(books=>res.json(books))
+        .catch(err=>res.status(400).json('Error: '+err));
+})
+
+
+
 
 export default bookRouter;
 
