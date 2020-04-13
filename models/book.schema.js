30c1d982edf922ddcd9aaa0ab808f40103ae997e
itachi1994 <you@example.com>
2020-04-13 01:00:47 -0400

    updated schema and api calls

--- models/book.schema.js
+++ models/book.schema.js
@@ -3,39 +3,12 @@ import {image} from './image.schema';
 import {price} from "./price.schema";
 
 export const BookSchema = new mongoose.Schema({
-        title: {
-            type: String,
-            required: true
-        },
-        authors: {
-            type: Array,
-            default: [],
-            required: true
-        },
-        publisher: String,
-
-        description: {
-            type: String,
-            required: true
-        },
         isbn: {
             type: Array,
             default: [],
             required: true
         },
-
-        pageCount: Number,
-
-        category: {
-            type: Array,
-            default: [],
-            required: true
-        },
-
-        averageRating: Number,
-        ratingsCount: Number,
-        image: image,
-        language: String,
+        quantity: Number,
         price: price,
         seller: String,
 
--- routes/book.js
+++ routes/book.js
@@ -10,13 +10,9 @@ const book = require('../models/book.schema')
 const bookRouter = express.Router();
 
 
-/*Post a book*/
+/*Post a book*/ 
 bookRouter.route('/Addbook').post(async (req, res) => {
     const body = req.body;
-    let checkBook = await book.find({title: body.title});
-    if (checkBook.length > 0) {
-        return res.send({status: "Book is already present"});
-    }
     const newBook = new book(body);
     newBook.save()
         .then(()=>res.json({status:'Book added!!'}))
@@ -39,13 +35,13 @@ bookRouter.route('/getBookById/:bid').get((req, res) => {
         .catch(err => res.status(400).json('Error: '+err))
 })
 
-bookRouter.route('/search').get((req, res) => {
-    let query = req.query.q;
-    book.find({$or:[{title: {"$regex": query,"$options":"i"}},
-            {authors:{"$elemMatch":{"$regex":query,"$options":"i"}}},{category:{"$elemMatch":{"$regex":query,"$options":"i"}}}]})
-        .then(books=>res.json(books))
-        .catch(err=>res.status(400).json('Error: '+err));
-})
+// bookRouter.route('/search').get((req, res) => {
+//     let query = req.query.q;
+//     book.find({$or:[{title: {"$regex": query,"$options":"i"}},
+//             {authors:{"$elemMatch":{"$regex":query,"$options":"i"}}},{category:{"$elemMatch":{"$regex":query,"$options":"i"}}}]})
+//         .then(books=>res.json(books))
+//         .catch(err=>res.status(400).json('Error: '+err));
+// })
 
 
 
