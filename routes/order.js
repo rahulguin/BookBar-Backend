5f23b757ce3791411f93e95749f7592aed24e6f2
Ritvik Vinodkumar <ritvikvinod@gmail.com>
2020-04-20 18:21:26 -0400

    get all orders for seller

--- routes/order.js
+++ routes/order.js
@@ -18,6 +18,11 @@ orderRoutes.route('/allOrders/:user').get((req,res)=>{
     order.find({buyer:user}).then(book=> res.json(book)).catch(err=> res.status(400).json(err));
 })
 
+orderRoutes.route('/allOrdersForSeller/:seller').get((req,res)=>{
+    const seller = req.params['seller'];
+    order.find({items: {$elemMatch:{"seller" : seller}}}).then(books=> res.json(books)).catch(err=> res.status(400).json(err));
+})
+
 // orderRouter.route('/delete/:user').delete((req,res)=>{
 //     const user = req.params['user'];
 //     console.log(user);
