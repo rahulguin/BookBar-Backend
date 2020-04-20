b0ae501ae1f4d95deb0f8450d2e230dc2767cb83
deepak <kumar.de@husky.neu.edu>
2020-04-19 22:38:36 -0400

    added order logic

--- routes/order.js
+++ routes/order.js
@@ -4,20 +4,18 @@ const order = require('../models/order.schema')
 
 const orderRoutes = express.Router();
 
-orderRoutes.route('/addToOrder/:user').put((req,res)=>{
+orderRoutes.route('/addToOrder/:user').post((req,res)=>{
     const body = req.body;
     const user = req.params['user'];
-    let options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    order.updateOne({"buyer": user}, {"$push": {
-        "items":body
-    }}, options).then(status=> res.send(status)).catch(err=> res.status(400).json(err));
+    const newOrder = new order({buyer:user, items:body})
+    newOrder.save().then(status=> res.send(status)).catch(err=> res.status(400).json(err));
 
 })
 
 
 orderRoutes.route('/allOrders/:user').get((req,res)=>{
     const user = req.params['user'];
-    order.find({buyer:user}).then(book=> res.json(book[0])).catch(err=> res.status(400).json(err));
+    order.find({buyer:user}).then(book=> res.json(book)).catch(err=> res.status(400).json(err));
 })
 
 // orderRouter.route('/delete/:user').delete((req,res)=>{
